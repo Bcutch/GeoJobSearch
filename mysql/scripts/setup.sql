d07b13e8ee050e0e79d499b10f4adc98b81552a7
aperezca <aperezca@uoguelph.ca>
2024-02-01 17:43:16 -0500

    Created database schema

--- mysql/scripts/setup.sql
+++ mysql/scripts/setup.sql
@@ -1,8 +1,15 @@
-use template_db;
+USE template_db;
 
-create table if not exists note
-(
-    id int auto_increment comment 'Primary Key'
-        primary key,
-    text varchar(255) null
+CREATE TABLE IF NOT EXISTS job (
+    id INT AUTO_INCREMENT COMMENT 'Primary Key' PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    company VARCHAR(255) NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    salary INT NOT NULL,
+    field VARCHAR(255) NOT NULL,
+    is_remote BOOLEAN NOT NULL DEFAULT FALSE,
+    latitude DECIMAL(11,8) NOT NULL,
+    longitude DECIMAL(11,8) NOT NULL
 );
