a2b9cf5868426b98c178b60edcd59df9ff479ea0
Nedas Kibildis <nedas11@icloud.com>
2024-02-01 16:14:14 -0500

    Fixed script to pull from indeed, set default link and number of scraped pages to 3

--- spring/src/main/python/ScrapingBot.py
+++ spring/src/main/python/ScrapingBot.py
@@ -1,26 +1,43 @@
 import time
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 
-url = "https://ca.indeed.com/jobs?q=python&l=Guelph%2C+ON&vjk=1f3e66712fb1ef37"
+# Variable That Gets Number Of Pages Scraped
+scrapedPages = 3
 
-driver = webdriver.Chrome()
-driver.get(url)
-driver.implicitly_wait(10)
-page_source = driver.page_source
+# Empty Array To Store Dictionaries With Job Data
+jobData = []
 
-# Close the browser window
-driver.quit()
+# Loop for going through each page and getting all 15 jobs information
+for i in range(scrapedPages):
+    # Dynamic url that uses the other last query to change pages, increases by 20 everytime which is what brings you to a new page
+    url = f"https://ca.indeed.com/jobs?q=python&l=&from=searchOnHP&vjk=1dbe12f243c824bf&start={i * 20}"
+    
+    # Launch Selenium Web Browser
+    driver = webdriver.Chrome()
+    # Open URL and wait for everything to load
+    driver.get(url)
+    driver.implicitly_wait(10)
+    # Get Dynamic url as page source for beautiufl soup to parse through the website
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, 'html.parser')
 
-# Now you can use BeautifulSoup to parse the page source
-soup = BeautifulSoup(page_source, 'html.parser')
-for element in soup.find_all('h2', class_='jobTitle'):
-    anchorTag = element.find('a')
-    if anchorTag:
-        link = anchorTag.get('href')
-        print(link)
-    print(element.text)
+    # Loop to go through each reference tag, and run the driver for that specific link so that you can get the active link
+    for element in soup.find_all('a', class_="jcs-JobTitle"):
+        href = element.get('href') # Get link that the a is referencing to 
+        driver.get("https:///ca.indeed.com/" + href) # Launch new driver with dynamic link
+        url = driver.current_url # Save the new url that opens as the link for our job title
+        jobSoup = BeautifulSoup(driver.page_source, 'html.parser') # Creates a new soup "Driver" for current page to parse through
+        title = jobSoup.find('h1', class_="jobsearch-JobInfoHeader-title").find('span').text # Code to get job title for given url
+        jobData.append({'title': f'{title}', 'url': f'{url}'}) # Push to dictionary
+
+    driver.quit()   # Close the browser window
+
+    i = 0
+    for job in jobData:
+        print(f'{i}: {job["title"]}: {job["url"]}')
+        i+=1
+
 
 
 
