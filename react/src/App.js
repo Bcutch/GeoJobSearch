5eb8ca92ea8048b0e274fb658cf0a5bbefe6f43e
Cooper MacGregor <cooper.macgregor14@gmail.com>
2024-02-03 17:13:02 -0500

    added search bar to search jobs

--- react/src/App.js
+++ react/src/App.js
@@ -1,12 +1,14 @@
 import DisplayJobs from "./components/DisplayJobs";
 import Filtering from "./components/Filtering";
 import NavigationBar from "./components/NavigationBar";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div>
       <NavigationBar />
       <main>
+        <Search />
         <Filtering />
         <DisplayJobs />
       </main>
