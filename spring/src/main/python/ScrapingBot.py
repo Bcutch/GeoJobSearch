542d919e68de0a41cbaf7e78557d20a01e083863
Nedas Kibildis <nedas11@icloud.com>
2024-02-12 18:13:02 -0500

    Finished Linked in scraper, and made both scraping scripts functions so that we dont always have to run both of them

--- spring/src/main/python/ScrapingBot.py
+++ spring/src/main/python/ScrapingBot.py
@@ -4,11 +4,11 @@ from bs4 import BeautifulSoup
 
 # Variable That Gets Number Of Pages Scraped
 scrapedPages = 3
-
+SCROLL_PAUSE_TIME = 2.5
 # Empty Array To Store Dictionaries With Job Data
 jobData = []
 
-def scrapeIndeed(numPages):
+def scrapeIndeed(numPages, jobData):
     # Loop for going through each page and getting all 15 jobs information
     for i in range(scrapedPages):
         # Dynamic url that uses the other last query to change pages, increases by 20 everytime which is what brings you to a new page
@@ -39,10 +39,40 @@ def scrapeIndeed(numPages):
         driver.quit()   # Close the browser window
 
 
-
-def scrapeLinkedIn():
+def scrapeLinkedIn(numPages, jobData):
     url="https://www.linkedin.com/jobs/search?position=1&pageNum=0"
     driver = webdriver.Chrome()
     # Open URL and wait for everything to load
     driver.get(url)
     driver.implicitly_wait(10)
+
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    pagesScraped = 0 #One page scraped equals one scroll to bottom due to infinite loading
+    
+    # Scrolls to bottom numPages times since linkedin uses infinite scrolling instead of pages
+
+    
+    while pagesScraped != numPages:
+        pagesScraped+= 1
+        # Scroll down to bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        # Wait to load page
+        time.sleep(SCROLL_PAUSE_TIME)
+
+        # Calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+    page_source = driver.page_source
+    soup = BeautifulSoup(page_source, 'html.parser')
+    print(soup)
+    driver.quit()
+
+    # Loop to find all reference tags
+    for element in soup.find_all('a', class_="base-card__full-link"):
+        title = element.find('span').text.strip() #title is printed with 3 new lines so use strip just to get title
+        url = element.get('href')
+        jobData.append({'title': f'{title}', 'url': f'{url}'}) # add to dictionary
