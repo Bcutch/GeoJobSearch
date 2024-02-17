68d14772724e6a26f6d06fba3ae48695d49f6268
Brettan Cutchall <bcutchal@uoguelph.ca>
2024-02-17 12:33:58 -0500

    added comments

--- spring/src/main/python/filterJob.py
+++ spring/src/main/python/filterJob.py
@@ -1,13 +1,16 @@
 import sys
 import mysql.connector
 
+#must be changed to connect to the database you want to use
 db = mysql.connector.connect(host="localhost", user="root", passwd="root", database="testdb")
 
 mycursor = db.cursor()
 
+#function to be called to return the dict with filtered jobs
 def filter(filterBy, filterArg):
 
     retDict = []
+#select statement using arguments
     mycursor.execute("""SELECT * FROM job WHERE %s = '%s';""" % (filterBy, filterArg))
 
     for i in mycursor:
