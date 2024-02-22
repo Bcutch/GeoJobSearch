4aecb2f83e30e36d4cebc1884a1a805ce9de4a2e
Jacob McKenna <jmcken10@uoguelph.ca>
2024-02-21 19:05:25 -0500

    Finished updating LinkedIn Scraper. It is a little bit faster but still quite slow. Also was unable to find remoteness data and some listings are salary, field, and job type. In exchange there is other data in the listing dictionary such as positingDate, seniority, and description.

--- spring/src/main/python/ScrapingBot.py
+++ spring/src/main/python/ScrapingBot.py
@@ -1,8 +1,7 @@
-import time, random
+import time
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
-random.seed(time.time())
 
 # Variable That Gets Number Of Pages Scraped
 scrapedPages = 3
@@ -46,6 +45,29 @@ def scrapeIndeed(numPages, jobData):
 
         driver.quit()   # Close the browser window
         
+        
+        
+#########################
+#   LINKEDIN SCRAPING   #
+#########################
+
+#
+# WARNING: THIS WILL TAKE A VERY LONG TIME approx 5-10 minutes per page
+#
+
+# Run by calling:
+# scrapeLinkedIn(numPages:int, jobData:list)
+#
+# jobData list will be filled with found jobs
+# 
+# upon calling this a chrome session will open (do not close this, it should close automatically when finished)
+# and job data will be scraped. The urls found will then be opened one by one in this chrome window and scraped for data.
+#
+# unfortunatly this cannot run much faster as if you scrape too fast, linkedIn will detect the scraping and temporarily
+# refuse your the connection to linkedIn
+# this code should be able to handle many of the errors however there are many many MANY possible ways this could go wrong.
+# good luck
+        
 
 
 def getSoupforLinkedIn(url:str, driver:webdriver.Chrome, options:webdriver.ChromeOptions = None, numPages:int = 0) -> BeautifulSoup:
@@ -54,27 +76,33 @@ def getSoupforLinkedIn(url:str, driver:webdriver.Chrome, options:webdriver.Chrom
     auth_wall sign in page after 3 quick consecutive searches. This function gets around this
     to ensure none of the data can be messed with.
 
+
     Args:
-        url (str): URL that will be searched
+        url (str): linkedIn URL that will be searched
         driver (webdriver.Chrome): webdriver that will be used
-        numPages(int): If true, will scroll for number of pages given. Default=0
+        options (webdriver.ChromeOptions, optional): options class that will be used in the webdriver. Defaults to None.
+        numPages (int, optional): number of pages, used to determine scroll height of page since linkedIn has endless scroll. Defaults to 0.
+
+    Raises:
+        ConnectionError: this is raised if cannot connect to linkedIn
+        TimeoutError: This is raised if there is a timeout while scrapping linkedIn
 
     Returns:
         BeautifulSoup: Returns the BeautifulSoup class if found. Returns None if nothing is found
     """
     
     # variables
-    soup = None
+    soup = None             # final return variable to return soup
     _options = options
-    if _options == None:
+    if _options == None:                    # set options if none exist
         _options = webdriver.ChromeOptions()
         _options.add_argument('log-level=3')     # only allows fatal errors to appear, prevents needless spam
     maxTimeoutAttempts = 10        # number of attempts to be made before a timeout error is raised
-    timeoutAttempts = 0
-    sleepSecondsLoaded = 3         # number of seconds to wait after a page loaded successfully
-    sleepSecondsAuth = 6            # number of seconds to wait after a page is auth wall
+    timeoutAttempts = 0         # number of attempts used before timeout
+    sleepSecondsLoaded = 2         # number of seconds to wait after a page loaded successfully
+    sleepSecondsAuth = 5            # number of seconds to wait after a page is auth wall
     pageKey = None                  # pagekey of loaded linkedIn page
-    authWallPrefix = "auth_wall"
+    authWallPrefix = "auth_wall"        # prefix in scraped HTML that determines file is authentication wall
     
     try:
         # Open URL and wait for everything to load
@@ -133,14 +161,10 @@ def getSoupforLinkedIn(url:str, driver:webdriver.Chrome, options:webdriver.Chrom
             # check for load error
             bodyError = soup.find('body')      
             if bodyError != None: bodyError = str(bodyError) == "<body></body>"     # if body is empty
-            # reset = random.choices((True, False), (0.1, 0.9))      # randomly reset driver or target will refuse connection at some point
-            reset = False
+
             # if net error or load error
-            if netError != None or bodyError == True or reset: 
+            if netError != None or bodyError == True: 
                 #  reload driver
-                if reset: print("Random Reset")
-                if netError: print("net error")
-                if bodyError: print("body error")
                 driver.quit()
                 driver = None
                 time.sleep(1)
@@ -151,9 +175,6 @@ def getSoupforLinkedIn(url:str, driver:webdriver.Chrome, options:webdriver.Chrom
                 time.sleep(1)
                 driver.get(url)
                 time.sleep(1)
-                # driver.refresh()
-                
-                # raise ConnectionError("found net error")
                 
 
             # continue until no auth_wall or timeout
@@ -161,16 +182,11 @@ def getSoupforLinkedIn(url:str, driver:webdriver.Chrome, options:webdriver.Chrom
                 driver.get(url)
                 time.sleep(sleepSecondsAuth)
     
-    except ConnectionRefusedError as cre:
+    except ConnectionRefusedError as cre:       # sometimes, linkedIn will directly refuse connection if scraping too much
         print(cre)
         print("LinkedIn Refused Connection, cannot get soup.")
     
     except Exception as Error:
-        # driver.quit()
-        f = open('soupResult.txt', "w", encoding="utf-8")
-        if soup != None: f.write(str(soup.find_all()))
-        # f.write(str(str(soup.find('body')) == "<body></body>"))
-        f.close()
         print(Error)
         raise ConnectionError("An Error occured while retrieving soup for linkedIn")
             
@@ -178,7 +194,19 @@ def getSoupforLinkedIn(url:str, driver:webdriver.Chrome, options:webdriver.Chrom
     
 
 
-def scrapeLinkedIn(numPages:int, jobData:list):
+def scrapeLinkedIn(numPages:int, jobData:list) -> None:
+    """ Scrapes linkedin Website and saves found jobs to jobData
+
+    Args:
+        numPages (int): number of pages to scrape from linkedIn
+        jobData (list): list of dictionaries that this function will fill
+
+    Raises:
+        ConnectionError: if anything goes wrong connecting to linkedIn, this will be raised
+        ModuleNotFoundError: if soup does not find anything, this will be raised
+        RuntimeError: General error. if anything goes wrong, this will be raised
+
+    """
     if numPages <= 0:   # should only scrape web if asked to scrap a positive non-zero number of pages
         return 
     linkedInUrl="https://www.linkedin.com/jobs/search?position=1&pageNum=0"
@@ -192,17 +220,18 @@ def scrapeLinkedIn(numPages:int, jobData:list):
     
     # Open URL and wait for everything to load
         
-    soup = getSoupforLinkedIn(url=linkedInUrl, driver=driver, numPages=numPages)
+    soup = getSoupforLinkedIn(url=linkedInUrl, driver=driver, options=options, numPages=numPages)
     if soup == None: raise ConnectionError("Could not get any info for linkedIn")
     
     try:
 
         # Loop to find all reference tags
         for jobListing in soup.find_all('div', class_='base-card'):
+            if jobListing == None: raise ModuleNotFoundError("jobListing should have been found")
             url = jobListing.find('a', class_='base-card__full-link').get('href')
             
             # get soup for more details info
-            detailsSoup = getSoupforLinkedIn(url=url, driver=driver)
+            detailsSoup = getSoupforLinkedIn(url=url, options=options, driver=driver)
             if detailsSoup == None: 
                 driver.quit()
                 return jobData
@@ -213,6 +242,7 @@ def scrapeLinkedIn(numPages:int, jobData:list):
             postingdate    = jobListing.find('time').get('datetime')       # gets date as format: yyyy-mm-dd
             company     = jobListing.find('h4', class_='base-search-card__subtitle').text.strip()
             
+            # open details from job page to get more info
             coreInfo     = detailsSoup.find_all('li', class_="description__job-criteria-item")
             description  = detailsSoup.find('div', 'show-more-less-html__markup')
             if description != None:
@@ -239,119 +269,18 @@ def scrapeLinkedIn(numPages:int, jobData:list):
             else:
                 salary = None
             
-            # f = open('demofileDetails.txt', "w", encoding="utf-8")
-            # f.write(str(detailsSoup.find_all()))
-            # f.close()
             
         
             jobData.append({'title': f'{title}', 'url': f'{url}', 'company':f'{company}', 
                             'location':f'{location}', 'postingdate':f'{postingdate}',
                             'jobType':jobType, 'field':f'{field}', 'salary':salary,
                             'seniority':seniority, 'description':description}) # add to dictionary
+    except Exception as error:
+        raise RuntimeError("An error occured while scraping LinkedIn")
     finally:
-        # count += 1
-        # if count == limit: break
         driver.quit()
 
-# def testFunc() -> None:
-#     timeoutSeconds = 20     # an error will be thrown if webdriver cannot load a page in 20 seconds
-#     start = time.time()
-#     auth_counter = 0
-#     good_counter = 0
-    
-#     waitSeconds_norm = 1            # wait time after a successful page find
-#     waitSeconds_auth = 3            # wait time after hit with auth page
-#     linkedInUrl = "https://www.linkedin.com/jobs/search?position=1&pageNum=0"
-#     badsite = "http://nasufaishfbaojkflabhigudhasbdfhosgvdhasihbgfafm.ca/"
-#     driver = webdriver.Chrome()
-#     driver.set_page_load_timeout(timeoutSeconds)
-#     # driver.implicitly_wait(10)
-#     timeoutOccured = False
-#     try:
-#         for i in range(10):
-#             driver.get(linkedInUrl)
-#             time.sleep(waitSeconds_norm)
-#             soup = BeautifulSoup(driver.page_source, 'html.parser')
-#             if soup == None: return None    # page not found
-#             meta = soup.find('meta')
-#             if meta != None: pageKey = meta.get("content")
-#             # continue refresh until no auth_wall
-#             if pageKey != None: print(pageKey[:9])
-#             timeoutCounter = 0
-#             while (pageKey != None and pageKey[:9] == 'auth_wall') or pageKey == None:
-#                 auth_counter += 1
-#                 timeoutCounter += 1
-#                 time.sleep(waitSeconds_auth)
-                
-#                 driver.get(linkedInUrl)
-#                 # driver.refresh()
-#                 soup = BeautifulSoup(driver.page_source, 'html.parser')
-#                 if soup == None: return None    # page not found
-#                 meta = soup.find('meta')
-#                 if meta != None: pageKey = meta.get("content")
-                
-#                 if timeoutCounter >= 10:
-#                     timeoutOccured = True
-#                     pageKey = "timeout"
-            
-#             if pageKey != None and pageKey != 'timeout':
-#                 good_counter += 1
-        
-#     except Exception as Error:
-#         print(Error)
-#     finally:
-#         driver.quit()
-        
-#     end = time.time()
-    
-#     print("elapsed: ",end-start)
-#     print(f"timeoutOccured: {timeoutOccured}\n")
-#     print("waitSeconds_norm:", waitSeconds_norm)
-#     print("waitSeconds_auth:", waitSeconds_auth)
-#     print(f"{good_counter}/{auth_counter} good/bad")
-#     print(f"{(end-start) / good_counter} seconds per good one")
-    
-#     # buildString = "------------------------\n"
-#     # buildString += f"Elapsed {end-start}\n"
-#     # buildString += f"timeoutOccured: {timeoutOccured}\n"
-#     # buildString += f"waitSeconds_norm: {waitSeconds_norm}\n"
-#     # buildString += f"waitSeconds_auth: {waitSeconds_auth}\n"
-#     # buildString += f"{good_counter}/{auth_counter} good/bad\n"
-#     # buildString += f"{(end-start) / good_counter} seconds per good one\n"
-    
-#     # print(buildString)
-#     # return buildString
-    
-    
-
-# myString = ""
-# for norm in range(1,6):
-#     for auth in range(1,6): 
-#         myString += testFunc(waitSeconds_norm=norm, waitSeconds_auth=auth)
-#         myString += "\n"
-
-# f = open('resultsofTesting.txt', "w", encoding="utf-8")
-# f.write(myString)
-# f.close()
-
-
 # tempData = []
-# testlinkedInUrl="https://www.linkedin.com/jobs/search?position=1&pageNum=0"
-# options = webdriver.ChromeOptions()
-# options.add_argument('log-level=3')     # only allows fatal errors to appear, prevents needless spam
-# testdriver = webdriver.Chrome(options=options)
-# getSoupforLinkedIn(url=testlinkedInUrl, driver=testdriver)
-# getSoupforLinkedIn(url=testlinkedInUrl, driver=testdriver)
-# getSoupforLinkedIn(url=testlinkedInUrl, driver=testdriver)
-# getSoupforLinkedIn(url=testlinkedInUrl, driver=testdriver)
-# getSoupforLinkedIn(url=testlinkedInUrl, driver=testdriver)
-# getSoupforLinkedIn(url=testlinkedInUrl, driver=testdriver)
-# getSoupforLinkedIn(url=testlinkedInUrl, driver=testdriver)
-# testdriver.quit()
-
 
 # scrapeLinkedIn(1,tempData)
 
-# f = open('linkedScrapedDataTest.txt', "w", encoding="utf-8")
-# f.write(str(tempData))
-# f.close()
