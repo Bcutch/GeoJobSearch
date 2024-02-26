d2bcff3e253097d4488456f085f6399a50495de8
Jacob McKenna <jmcken10@uoguelph.ca>
2024-02-26 17:56:48 -0500

    continued rebasing

--- spring/src/test/python/test_scraperToData.py
+++ spring/src/test/python/test_scraperToData.py
@@ -62,15 +62,24 @@ def testTableCreated():
     assert connection.tableExists(f'{scraperToData.tablename}') == True        # tablename should exist 
     
 def testAddCorruptData():
-    with pytest.raises(LookupError):
-        badData = [{'THIS':'IS A TEST'}]
-        connection = scraperToDataConnection()
+    badData = [{'THIS':'IS A TEST'}]
+    connection = scraperToDataConnection()
+    
+    with pytest.raises(KeyError):
         connection.addJobData(badData)
 
 def testAddMultipleData():
     data = [{   # this is fake data that will be inserted into the table and also removed
         'title':fakeDataTitle,
-        'url':'THIS_IS_FAKE_DATA'
+        'url':'THIS_IS_FAKE_DATA',
+        'company': 'Shark Hunters',
+        'location': "Atlantis",
+        'postingdate': "3020-12-31",
+        'jobType': "Full-Time",
+        'field': "Fishing",
+        'salary': "$200,000 - $300,000 per year",
+        'seniority': "High-level",
+        'description': "The \tcraziest job you'll ever see!!\n\n."
         }]
     connection = scraperToDataConnection()
     connection.addJobData(data)     # we will add this same data multiple times to check for duplicates
@@ -82,7 +91,6 @@ def testAddMultipleData():
                               WHERE title = '{fakeDataTitle}';
                               """)
     result = connection.cursor.fetchall()
-    # cur.get_attributes()
     assert len(result) == 1      # there should not be any duplicate entries but there should be the one that was added
     
     connection.cursor.execute(f"""DELETE FROM {scraperToData.tablename} WHERE title='{data[0]['title']}'""")    
@@ -94,12 +102,99 @@ def testLongUrlLength():
         'title':fakeDataTitle,
         'url':string
         }]
+    connection = scraperToDataConnection()
     
     with pytest.raises(mysql.connector.DataError):
-        connection = scraperToDataConnection()
-        connection.addJobData(data)
+        connection.addJobData(data)                 # should not be able to add a value longer than the allowed length
 
     connection.cursor.execute(f"""DELETE FROM {scraperToData.tablename} WHERE title='{data[0]['title']}'""")    
+    connection.database.commit()                # clear table of fake data
+    
+def testNoTitle(): # title value MUST exist, if not an error should be raised
+    data = [{   # this is fake data that will be inserted into the table and also removed
+        'url':'THIS_IS_FAKE_DATA'
+        }]
+    connection = scraperToDataConnection()
+    
+    with pytest.raises(KeyError):
+        connection.addJobData(data)
+    
+    connection.cursor.execute(f"""DELETE FROM {scraperToData.tablename} WHERE url='{data[0]['url']}'""")    
+    connection.database.commit()                # clear table of fake data
+    
+    
+def testNoURL(): # url value MUST exist, if not an error should be raised
+    data = [{   # this is fake data that will be inserted into the table and also removed
+        'title':fakeDataTitle
+        }]
+    connection = scraperToDataConnection()
+    
+    with pytest.raises(KeyError):
+        connection.addJobData(data)
+    
+    connection.cursor.execute(f"""DELETE FROM {scraperToData.tablename} WHERE title='{data[0]['title']}'""")    
+    connection.database.commit()                # clear table of fake data
+
+def testEmptyData():
+    emptyData = []
+    
+    connection = scraperToDataConnection()
+    # with pytest.raises(IndexError):
+    connection.addJobData(emptyData)
+    
+    connection.cursor.execute(f"""
+                              SELECT * FROM {scraperToData.tablename}
+                              WHERE title = '{fakeDataTitle}';
+                              """)
+    result = connection.cursor.fetchall()
+    assert len(result) == 0      # there should not be any entries
+    
+    if len(result) > 0:
+        connection.cursor.execute(f"""DELETE FROM {scraperToData.tablename} WHERE title='{emptyData[0]['title']}'""")    
+        connection.database.commit()                # clear table of fake data
+    
+    
+def testAddWrongRemoteValue():
+    # remoteness should only be 0 or 1
+    remoteData = [{   # this is fake data that will be inserted into the table and also removed
+        'title':fakeDataTitle,
+        'url':'THIS_IS_FAKE_DATA',
+        'remote': "Hybrid"              # should be a bool, not string
+        }]
+    connection = scraperToDataConnection()
+    with pytest.raises(ValueError):
+        connection.addJobData(remoteData)
+    
+    connection.cursor.execute(f"""DELETE FROM {scraperToData.tablename} WHERE title='{remoteData[0]['title']}'""")    
+    connection.database.commit()                # clear table of fake data
+
+def testAddCorrectRemoteValue():
+    # remoteness should add correctly
+    
+    remoteData = [{   # this is fake data that will be inserted into the table and also removed
+        'title':fakeDataTitle,
+        'url':'THIS_IS_FAKE_DATA',
+        'remote': False              # should be a bool, not string
+        }]
+    connection = scraperToDataConnection()
+    connection.addJobData(remoteData)
+    
+    connection.cursor.execute(f"""DELETE FROM {scraperToData.tablename} WHERE title='{remoteData[0]['title']}'""")    
+    connection.database.commit()                # clear table of fake data
+    
+def testHandleExtraData():
+    # should not add or try to add extra data if there is in the dictionary
+    extraData = [{   # this is fake data that will be inserted into the table and also removed
+        'title':fakeDataTitle,
+        'url':'THIS_IS_FAKE_DATA',
+        # assuming the following entries will never have a column in the database
+        'cat': 'yes',
+        'dog': 'no'
+        }]
+    connection = scraperToDataConnection()
+    connection.addJobData(extraData)            # should raise no errors
+    
+    connection.cursor.execute(f"""DELETE FROM {scraperToData.tablename} WHERE title='{extraData[0]['title']}'""")    
     connection.database.commit()                # clear table of fake data
     
     
