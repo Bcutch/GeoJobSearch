1e4a547a80b41bf1fabcf5966a9d67d50ae03c3e
Cooper MacGregor <cooper.macgregor14@gmail.com>
2024-02-25 22:03:01 -0500

    everything works within docker env, get route gets mock data from db, docker now runs spring server with docker compose up

--- spring/src/main/java/spring/backend/JobController.java
+++ spring/src/main/java/spring/backend/JobController.java
@@ -1,5 +1,6 @@
 package spring.backend.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -9,6 +10,7 @@ public class JobController {
 
     private final JobRepository jobRepository;
 
+    @Autowired
     public JobController(JobRepository jobRepository) {
         this.jobRepository = jobRepository;
     }
--- spring/src/main/java/spring/backend/models/JobRepository.java
+++ spring/src/main/java/spring/backend/models/JobRepository.java
@@ -2,6 +2,6 @@ package spring.backend.models;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface JobRepository extends JpaRepository<Job, Long> {
+public interface JobRepository extends JpaRepository<Job, Integer> {
     // Custom query methods can be added here
 }
