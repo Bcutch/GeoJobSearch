8811ee1050f48d7be23c39b3c272c8d2c03bf4ad
Cooper MacGregor <cooper.macgregor14@gmail.com>
2024-02-26 20:24:14 -0500

    fixed display jobs for merge

--- react/src/components/DisplayJobs.jsx
+++ react/src/components/DisplayJobs.jsx
@@ -1,15 +1,4 @@
-import React, {useState, useEffect} from "react";
-
-const exampleJobs = [
-  {
-    title: "software dev",
-    link: "http://www.google.com",
-  },
-  {
-    title: "test2",
-    link: "link2",
-  },
-];
+import React, { useState, useEffect } from "react";
 
 const DisplayJobs = () => {
   const [jobs, setJobs] = useState([]);
@@ -24,25 +13,26 @@ const DisplayJobs = () => {
   return (
     <div className="flex justify-center my-4">
       <div className="flex flex-col gap-4">
-        {exampleJobs.map((job, index) => ( /* was jobs.map instead of exampleJobs.map */
-          <div key={index} className="border p-4 rounded-lg">
-            <h2>{job.title}</h2>
-            <p>Company: {job.company}</p>
-            <p>Location: {job.location}</p>
-            <p>Description: {job.description}</p>
-            <a href={job.link} target="_blank" rel="noopener noreferrer">
-              Apply here
-            </a>
-            <p>Salary: {job.salary}</p>
-            <p>Field: {job.field}</p>
-            <p>Remote: {job.is_remote ? "Yes" : "No"}</p>
-            {/* You can add latitude and longitude if needed */}
-          </div>
-        ))}
+        {jobs.map(
+          (job, index /* was jobs.map instead of exampleJobs.map */) => (
+            <div key={index} className="border p-4 rounded-lg">
+              <h2>{job.title}</h2>
+              <p>Company: {job.company}</p>
+              <p>Location: {job.location}</p>
+              <p>Description: {job.description}</p>
+              <a href={job.link} target="_blank" rel="noopener noreferrer">
+                Apply here
+              </a>
+              <p>Salary: {job.salary}</p>
+              <p>Field: {job.field}</p>
+              <p>Remote: {job.is_remote ? "Yes" : "No"}</p>
+              {/* You can add latitude and longitude if needed */}
+            </div>
+          )
+        )}
       </div>
     </div>
   );
 };
 
 export default DisplayJobs;
-
