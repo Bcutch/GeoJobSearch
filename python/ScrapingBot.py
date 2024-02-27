d0ef015fb094473513feb0cb68c69bbcc8bf385d
Nedas Kibildis <nedas11@icloud.com>
2024-02-26 19:56:17 -0500

    Converged scraperToData into Scraping Bot File So that 2 files don't need to be ran when building. This fixed issue of database not being created on build

--- python/ScrapingBot.py
+++ python/ScrapingBot.py
@@ -1,5 +1,6 @@
 import time, json
 from selenium import webdriver
+import mysql.connector
 from bs4 import BeautifulSoup
 
 # Sleep for 1 Minute So That Python doesn't try to connect to the selenium server before it is established
@@ -399,9 +400,48 @@ def scrapeLinkedIn(numPages:int, jobData:list, jobLimit:int = -1) -> None:
     finally:
         driver.quit()
 
-tempData = []
-scrapeIndeed(1, tempData, jobLimit=10)
-for element in tempData:
+jobDict = []
+scrapeIndeed(1, jobDict, jobLimit=10)
+
+for element in jobDict:
     print(element['title'])
+
+    db = mysql.connector.connect(
+    host="mysql",  # Updated to the new host
+    user="root",  # DB_USER
+    passwd="pwd",  # DB_PASSWORD
+    database="template_db"  # DB_DATABASE
+)
+
+
+mycursor = db.cursor()
+
+# mycursor.execute("DROP TABLE job")
+
+print('Creating Table')
+
+mycursor.execute("""CREATE TABLE IF NOT EXISTS job (
+    id INT AUTO_INCREMENT COMMENT 'Primary Key' PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    company VARCHAR(255),
+    location VARCHAR(255),
+    description TEXT,
+    url VARCHAR(2000) NOT NULL,
+    salary INT,
+    field VARCHAR(255),
+    is_remote BOOLEAN NOT NULL DEFAULT FALSE,
+    latitude DECIMAL(11,8),
+    longitude DECIMAL(11,8)
+);""")
+
+print('table Created')
+
+
+for job in jobDict:
+    print(job)
+    mycursor.execute("""INSERT INTO job (title, url) VALUES ("%s", "%s");""" % (job['title'], job['url']))
+    db.commit()
+
+db.close()
 
 
