64a7bad8e9bb230d02c4311b96e17d28bd7e9b7c
Cooper <cooper.macgregor14@gmail.com>
2024-03-04 19:26:10 -0500

    added test double to test db in spring

---
+++ spring/src/test/java/spring/backend/testDatabase.java.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.DirtiesContext;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DirtiesContext // Use this if you want to reset the Spring ApplicationContext after this test class
+public class DatabaseConnectionTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    void testConnection() {
+        // This query is almost universally supported and doesn't depend on your schema
+        String version = jdbcTemplate.queryForObject("SELECT VERSION();", String.class);
+        assertThat(version).isNotNull();
+        System.out.println("Database Version: " + version);
+    }
+}
+
