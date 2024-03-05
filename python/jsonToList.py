35e5c6e38bad82013e447f82463a6db9d318eafe
Jacob McKenna <jmcken10@uoguelph.ca>
2024-03-04 22:35:15 -0500

    added python testings doubles via json file and python file to load the json information

---
+++ python/jsonToList.py
@@ -0,0 +1,21 @@
+import json
+
+# this file contains the function(s) used to retrieved the test doubles for job listings
+# this can be retrieved by calling the function, getSavedJobListings()
+# this will return the list of job dictionaries containing the job posting data to use
+
+# WARNING: IF THE jobListings.json FILE DIRECTORY CHANGES, THIS FILE WON'T WORK
+
+def getSavedJobListings() -> list:
+    """ converts json joblistings to list
+git 
+    Returns:
+        list: list of dictionary containing job listing data
+    """
+    
+    jobListingsPath = "python/jobListings.json"
+    jobData = []
+    
+    with open(jobListingsPath, "r") as fp:
+        jobData = json.load(fp)
+    return jobData
