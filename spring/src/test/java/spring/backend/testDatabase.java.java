0d6282e63ed25a1cceb909ba7cd2fb73e28096c8
Adam Fielding <afield01@uoguelph.ca>
2024-03-06 23:29:37 -0500

    Revert "Pulled newest additions from main and fixed TestDatabase to work with Jacoco"
    This reverts commit 62201deb98716ee852e04ff0781b995283d0d5cc

--- spring/src/test/java/spring/backend/testDatabase.java.java
+++ spring/src/test/java/spring/backend/testDatabase.java.java
@@ -1,4 +1,3 @@
-package spring.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @DirtiesContext // Use this if you want to reset the Spring ApplicationContext after this test class
-public class TestDatabase {
+public class DatabaseConnectionTest {
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
