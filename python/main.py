f9f377dd4138506548514b0a6db545235d9755e9
Jacob McKenna <jmcken10@uoguelph.ca>
2024-03-11 20:28:57 -0400

    Fixed scraping bot testing to use a local selenium statalone server. This allows testing to be done with the remote driver.

---
+++ python/main.py
@@ -0,0 +1,25 @@
+
+# This is the script that is called from the docker file and will run every time
+# docker compose is called
+# imports
+import ScrapingBot
+import scraperToData
+import time
+
+# Sleep for 1 Minute So That Python doesn't try to connect to the selenium server before it is established
+time.sleep(60)
+
+# scrape 10 jobs off of indeed
+jobDict = []
+ScrapingBot.scrapeIndeed(1, jobDict, jobLimit=10)
+
+# display jobs for testing purposes
+for element in jobDict:
+    print(element['title'])
+    
+# connect to mySQL job table
+connection = scraperToData.scraperToDataConnection(host="mysql", user="root", passwd="pwd", databaseName="template_db", dropTable=True)
+print("connected to db")
+
+# add jobs to table
+connection.addJobData(jobDict)
