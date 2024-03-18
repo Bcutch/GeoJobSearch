0a3e7cd5e9b5ff7bec2fcc2358cf4665a5a13c00
Nedas Kibildis <nedas11@icloud.com>
2024-03-18 18:13:23 -0400

    extended array so that index 8 and 9 exists

--- python/scraperToData.py
+++ python/scraperToData.py
@@ -210,7 +210,9 @@ class scraperToDataConnection:
                     loc = Nominatim(user_agent="Geopy Library")
                     getLoc = loc.geocode(job['location'])
                     if getLoc != None: # Only set longitude and Latitude if a valid value is found for given location
-                        values[8] = getLoc.longitude
+                        if len(values) < 9:  # Ensure that values has enough elements
+                            values.extend([None] * (9 - len(values) + 1))
+                        values[8] = getLoc.longitud
                         values[9] = getLoc.latitude
                         print(f"Location: {job['location']}, Longitude: {values[8]}, Latitude: {values[9]}")
 
