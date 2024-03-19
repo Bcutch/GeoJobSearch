1e3e92d03ea6c7cdc31462c6613cf7a7a17e671c
Brettan Cutchall <bcutchal@uoguelph.ca>
2024-03-18 20:17:59 -0400

    fixed to support multiple filtering options selected

--- spring/src/main/java/spring/backend/JobController.java
+++ spring/src/main/java/spring/backend/JobController.java
@@ -4,7 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import spring.backend.models.FilterOptions;
+import spring.backend.models.Job;
+import spring.backend.models.JobRepository;
+
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.io.*;
 
@@ -23,64 +30,96 @@ public class JobController {
         return jobRepository.findAll();
     }
 
+    private List<Job> combineResults(List<Job> first, List<Job> second) {
+        List<Job> ret = new ArrayList<>();
+
+        for (Job job : first) {
+            for (Job jobComp : second) {
+                if (job.getId() == jobComp.getId()) {
+                    ret.add(job);
+                    break;
+                }
+            }
+        }
+
+        return ret;
+    }
+
     @PutMapping("/jobs")
     public List<Job> filterJobs(@RequestBody FilterOptions filterOptions) { //Takes JSON data from request and puts it into filterOptions object
+        List<Job> jobTypeList = new ArrayList<>();
+        List<Job> distanceList = new ArrayList<>();
+        List<Job> salaryList = new ArrayList<>();
+        List<Job> remoteList = new ArrayList<>();
+        
         if (!filterOptions.getJobType().equals("Job Type")) {
             if (filterOptions.getJobType().equals("Full-time")) {
-                return jobRepository.findAll();
+                jobTypeList.addAll(jobRepository.findAll());
             }
             if (filterOptions.getJobType().equals("Part-time")) {
-                return jobRepository.findAll();
+                jobTypeList.addAll(jobRepository.findAll());
             }
             if (filterOptions.getJobType().equals("Internship")) {
-                return jobRepository.findAll();
+                jobTypeList.addAll(jobRepository.findAll());
             }
+        } else {
+            jobTypeList.addAll(jobRepository.findAll());
         }
+
         if (!filterOptions.getDistance().equals("Distance")) {
             if (filterOptions.getDistance().equals(">20km")) {
-                return jobRepository.findAll();
+                distanceList.addAll(jobRepository.findAll());
             }
             if (filterOptions.getDistance().equals(">50km")) {
-                return jobRepository.findAll();
+                distanceList.addAll(jobRepository.findAll());
             }
             if (filterOptions.getDistance().equals(">100km")) {
-                return jobRepository.findAll();
+                distanceList.addAll(jobRepository.findAll());
             }
             if (filterOptions.getDistance().equals(">150km")) {
-                return jobRepository.findAll();
+                distanceList.addAll(jobRepository.findAll());
             }
+        } else {
+            distanceList.addAll(jobRepository.findAll());
         }
+
         if (!filterOptions.getSalary().equals("Salary")) {
             if (filterOptions.getSalary().equals("$0-$50k")) {
-                return jobRepository.findBySalaryBetween(0, 50000);
+                salaryList.addAll(jobRepository.findBySalaryBetween(0, 50000));
             }
             if (filterOptions.getSalary().equals("$50k-$100k")) {
-                return jobRepository.findBySalaryBetween(50000, 100000);
+                salaryList.addAll(jobRepository.findBySalaryBetween(50000, 100000));
             }
             if (filterOptions.getSalary().equals("$100k-$150k")) {
-                return jobRepository.findBySalaryBetween(100000, 150000);
+                salaryList.addAll(jobRepository.findBySalaryBetween(100000, 150000));
             }
             if (filterOptions.getSalary().equals("$150k-$200k")) {
-                return jobRepository.findBySalaryBetween(150000, 200000);
+                salaryList.addAll(jobRepository.findBySalaryBetween(150000, 200000));
             }
             if (filterOptions.getSalary().equals("$200k+")) {
-                return jobRepository.findBySalaryGreaterThan(200000);
+                salaryList.addAll(jobRepository.findBySalaryGreaterThan(200000));
             }
+        } else {
+            salaryList.addAll(jobRepository.findAll());
         }
 
         if (!filterOptions.getRemote().equals("Remoteness")) {
             if (filterOptions.getRemote().equals("Remote")) {
-                return jobRepository.findByIsRemoteTrue();
+                remoteList.addAll(jobRepository.findByIsRemoteTrue());
             }
             if (filterOptions.getRemote().equals("Hybrid")) {
-                return jobRepository.findByIsRemoteTrue();
+                remoteList.addAll(jobRepository.findByIsRemoteTrue());
             }
             if (filterOptions.getRemote().equals("On-Site")) {
-                return jobRepository.findByIsRemoteFalse();
+                remoteList.addAll(jobRepository.findByIsRemoteFalse());
             }
+        } else {
+            remoteList.addAll(jobRepository.findAll());
         }
 
-        return jobRepository.findAll();
+        List<Job> firstCombine = combineResults(salaryList, remoteList);
+        List<Job> secondCombine = combineResults(distanceList, jobTypeList);
+        return combineResults(firstCombine, secondCombine);
     }
     
 }
